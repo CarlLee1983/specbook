a18b60a1c0814e81647beb9f3a9c09aca6bf8e1a
carl <yashino538@gmail.com>
2026-05-16 00:14:24 +0800

    feat: [docs] expose specbook/docs subpath export

---
+++ src/docs/index.ts
@@ -0,0 +1,12 @@
+export { validateUserDocs } from './validator.js'
+export type { ValidationResult, ValidationError } from './validator.js'
+export { scaffoldUserDocs } from './scaffold.js'
+export type { ScaffoldOptions, ScaffoldResult } from './scaffold.js'
+export { buildUserDocs } from './build.js'
+export type { BuildOptions, BuildResult } from './build.js'
+export {
+  resolveCoverage,
+  normalizeCoverageFlag,
+  ALL_CATEGORIES,
+  OVERVIEW_KEY,
+} from './coverage.js'
